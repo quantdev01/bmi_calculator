21f04ef324a661a748324e08fb5cb53a4e2cf20e
Daniel Kiyiki <danielngolu154@gmail.com>
2024-02-09 22:40:10 +0200

    initial commit

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -17,67 +17,64 @@ class _InputPageState extends State<InputPage> {
         ),
         body: Column(
           children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: const EdgeInsets.all(15),
-                    height: 170,
-                    decoration: BoxDecoration(
-                      color: const Color(0xff1d1e33),
-                      borderRadius: BorderRadius.circular(10),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff1d1e33),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: const EdgeInsets.all(10),
-                    height: 175,
-                    decoration: BoxDecoration(
-                      color: const Color(0xff1d1e33),
-                      borderRadius: BorderRadius.circular(10),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff1d1e33),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             Expanded(
               child: Container(
                 margin: const EdgeInsets.all(10),
-                height: 175,
                 decoration: BoxDecoration(
                   color: const Color(0xff1d1e33),
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
             ),
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: const EdgeInsets.all(20),
-                    height: 175,
-                    decoration: BoxDecoration(
-                      color: const Color(0xff1d1e33),
-                      borderRadius: BorderRadius.circular(10),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff1d1e33),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: const EdgeInsets.all(10),
-                    height: 175,
-                    decoration: BoxDecoration(
-                      color: const Color(0xff1d1e33),
-                      borderRadius: BorderRadius.circular(10),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff1d1e33),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ));
